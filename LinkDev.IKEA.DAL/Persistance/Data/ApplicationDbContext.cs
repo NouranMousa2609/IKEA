b1a92ae51c4ae5922377812d93e8bf8fe20149d2
NouranMousa2609 <nouranmousa55@gmail.com>
2024-09-21 20:20:05 +0300

    Part 03 Employee Module - Entities, Configs, Migration

--- LinkDev.IKEA.DAL/Persistance/Data/ApplicationDbContext.cs
+++ LinkDev.IKEA.DAL/Persistance/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using LinkDev.IKEA.DAL.Entities.Departments;
+using LinkDev.IKEA.DAL.Entities.Employees;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -25,5 +26,6 @@ namespace LinkDev.IKEA.DAL.Persistance.Data
         }
 
         public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
     }
 }
