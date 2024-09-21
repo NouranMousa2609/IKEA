d3011312e9afc7d818dac04a4d7ed73f6ea980e4
NouranMousa2609 <nouranmousa55@gmail.com>
2024-09-21 20:50:07 +0300

    Part 04 Employee Module - Repository

---
+++ LinkDev.IKEA.DAL/Persistance/Repositories/_Generic/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using LinkDev.IKEA.DAL.Entities;
+using LinkDev.IKEA.DAL.Entities.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Persistance.Repositories._Generic
+{
+    public interface IGenericRepository<T> where T : ModelBase
+    {
+        IEnumerable<T> Get(bool withAsNoTracking = true);
+
+        IQueryable<T> GetAllASIQueryable();
+        T? Get(int id);
+
+        int Add(T entity);
+
+        int Update(T entity);
+
+        int Delete(T entity);
+    }
+}
